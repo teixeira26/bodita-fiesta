aa64be69a19a8ff216b401daf7f11a6b20250f49
MatheitsonZilla <mteixeira@digichanges.com>
2023-01-01 23:07:02 -0300

    ksaoks

--- src/Components/ConfirmarAsistencia/ConfirmarAsistencia.jsx
+++ src/Components/ConfirmarAsistencia/ConfirmarAsistencia.jsx
@@ -7,7 +7,7 @@ const ConfirmarAsistencia =()=>{
             <div className="asistenciaContainer">
             <h4>CONFIRMACIÓN DE ASISTENCIA</h4>
             <p>Esperamos que seas parte de este gran día para nosotros.</p>
-                <p>Al finalizar la ceremonia realizaremos un compartir comunitário, te invitamos que te sumes y que colabores con algo rico.</p>
+                <p>Al finalizar la ceremonia realizaremos un compartir comunitário, te invitamos que te sumes y colabores con algo rico.</p>
             <Button template='verde' texto='CONFIRMAR ASISTENCIA' url='https://docs.google.com/forms/d/e/1FAIpQLSfRrAI9JJBuhBUM1KKov3qNDilaXA6yHW_mEdLRRtVFLPb6ag/viewform?usp=sf_link'></Button>
             </div>
         </section>
